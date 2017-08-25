d07315003dc786fac795e8d1c707e70c05b01424
lijin <lijin@vsoontech.com>
2017-08-25 14:55:17 +0800

    Add install script

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+TOP=$(pwd)
+
+cd $TOP/../
+
+ln -sf ./vim_plugins/AfterColors
+ln -sf ./vim_plugins/Align
+ln -sf ./vim_plugins/calendar-vim
+ln -sf ./vim_plugins/CountJump
+ln -sf ./vim_plugins/cvim
+ln -sf ./vim_plugins/fencview
+ln -sf ./vim_plugins/FindMate
+ln -sf ./vim_plugins/mark.vim
+ln -sf ./vim_plugins/matchit
+# ln -sf ./vim_plugins/omnicppcomplete
+ln -sf ./vim_plugins/project
+ln -sf ./vim_plugins/python_match
+ln -sf ./vim_plugins/repeat
+ln -sf ./vim_plugins/showmarks
+ln -sf ./vim_plugins/surround
+ln -sf ./vim_plugins/taglist
+ln -sf ./vim_plugins/vim-autocomplpop
+ln -sf ./vim_plugins/vim-fuzzyfinder
+ln -sf ./vim_plugins/vim-l9
+ln -sf ./vim_plugins/ZoomWin
+
+
+
