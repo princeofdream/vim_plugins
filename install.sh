cbbd7ec10137babd01f05f333cae299840e7aa53
lijin <lijin@vsoontech.com>
2017-11-17 14:03:27 +0800

    update install and uninstall script

--- install.sh
+++ install.sh
@@ -4,26 +4,26 @@ TOP=$(pwd)
 
 cd $TOP/../
 
-cp -r ./vim_plugins/AfterColors
-cp -r ./vim_plugins/Align
-cp -r ./vim_plugins/calendar-vim
-cp -r ./vim_plugins/CountJump
-cp -r ./vim_plugins/cvim
-cp -r ./vim_plugins/fencview
-cp -r ./vim_plugins/FindMate
-cp -r ./vim_plugins/mark.vim
-cp -r ./vim_plugins/matchit
-# cp -r ./vim_plugins/omnicppcomplete
-cp -r ./vim_plugins/project
-cp -r ./vim_plugins/python_match
-cp -r ./vim_plugins/repeat
-cp -r ./vim_plugins/showmarks
-cp -r ./vim_plugins/surround
-cp -r ./vim_plugins/taglist
-cp -r ./vim_plugins/vim-autocomplpop
-cp -r ./vim_plugins/vim-fuzzyfinder
-cp -r ./vim_plugins/vim-l9
-cp -r ./vim_plugins/ZoomWin
+cp -r ./vim_plugins/AfterColors ./
+cp -r ./vim_plugins/Align ./
+cp -r ./vim_plugins/calendar-vim ./
+cp -r ./vim_plugins/CountJump ./
+cp -r ./vim_plugins/cvim ./
+cp -r ./vim_plugins/fencview ./
+cp -r ./vim_plugins/FindMate ./
+cp -r ./vim_plugins/mark.vim ./
+cp -r ./vim_plugins/matchit ./
+# cp -r ./vim_plugins/omnicppcomplete ./
+cp -r ./vim_plugins/project ./
+cp -r ./vim_plugins/python_match ./
+cp -r ./vim_plugins/repeat ./
+cp -r ./vim_plugins/showmarks ./
+cp -r ./vim_plugins/surround ./
+cp -r ./vim_plugins/taglist ./
+cp -r ./vim_plugins/vim-autocomplpop ./
+cp -r ./vim_plugins/vim-fuzzyfinder ./
+cp -r ./vim_plugins/vim-l9 ./
+cp -r ./vim_plugins/ZoomWin ./
 
 
 
---
+++ uninstall.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+TOP=$(pwd)
+
+cd $TOP/../
+
+rm -rf ./AfterColors
+rm -rf ./Align
+rm -rf ./calendar-vim
+rm -rf ./CountJump
+rm -rf ./cvim
+rm -rf ./fencview
+rm -rf ./FindMate
+rm -rf ./mark.vim
+rm -rf ./matchit
+# rm -rf ./omnicppcomplete
+rm -rf ./project
+rm -rf ./python_match
+rm -rf ./repeat
+rm -rf ./showmarks
+rm -rf ./surround
+rm -rf ./taglist
+rm -rf ./vim-autocomplpop
+rm -rf ./vim-fuzzyfinder
+rm -rf ./vim-l9
+rm -rf ./ZoomWin
+
+
+
