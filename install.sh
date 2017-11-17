a023c46e6b77beb5fe0150009737f8af3243f778
JamesW <princetemp@outlook.com>
2017-11-17 13:49:29 +0800

    Change ln to cp

--- install.sh
+++ install.sh
@@ -4,26 +4,26 @@ TOP=$(pwd)
 
 cd $TOP/../
 
-ln -sf ./vim_plugins/AfterColors
-ln -sf ./vim_plugins/Align
-ln -sf ./vim_plugins/calendar-vim
-ln -sf ./vim_plugins/CountJump
-ln -sf ./vim_plugins/cvim
-ln -sf ./vim_plugins/fencview
-ln -sf ./vim_plugins/FindMate
-ln -sf ./vim_plugins/mark.vim
-ln -sf ./vim_plugins/matchit
-# ln -sf ./vim_plugins/omnicppcomplete
-ln -sf ./vim_plugins/project
-ln -sf ./vim_plugins/python_match
-ln -sf ./vim_plugins/repeat
-ln -sf ./vim_plugins/showmarks
-ln -sf ./vim_plugins/surround
-ln -sf ./vim_plugins/taglist
-ln -sf ./vim_plugins/vim-autocomplpop
-ln -sf ./vim_plugins/vim-fuzzyfinder
-ln -sf ./vim_plugins/vim-l9
-ln -sf ./vim_plugins/ZoomWin
+cp -r ./vim_plugins/AfterColors
+cp -r ./vim_plugins/Align
+cp -r ./vim_plugins/calendar-vim
+cp -r ./vim_plugins/CountJump
+cp -r ./vim_plugins/cvim
+cp -r ./vim_plugins/fencview
+cp -r ./vim_plugins/FindMate
+cp -r ./vim_plugins/mark.vim
+cp -r ./vim_plugins/matchit
+# cp -r ./vim_plugins/omnicppcomplete
+cp -r ./vim_plugins/project
+cp -r ./vim_plugins/python_match
+cp -r ./vim_plugins/repeat
+cp -r ./vim_plugins/showmarks
+cp -r ./vim_plugins/surround
+cp -r ./vim_plugins/taglist
+cp -r ./vim_plugins/vim-autocomplpop
+cp -r ./vim_plugins/vim-fuzzyfinder
+cp -r ./vim_plugins/vim-l9
+cp -r ./vim_plugins/ZoomWin
 
 
 
