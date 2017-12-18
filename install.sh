0a850e719910879144b9bf98431d223c2e236c0a
James <howpigcanfly@outlook.com>
2017-12-18 23:05:00 +0800

    Update install scripte

--- install.sh
+++ install.sh
@@ -2,28 +2,60 @@
 
 TOP=$(pwd)
 
+WHOAMI=`uname |grep -i Linux`
+SYSTEM_TYPE="Unkonw"
+
+if [ ! -z "$WHOAMI" ]
+then
+	echo "Using Linux"
+	SYSTEM_TYPE="Linux"
+fi
+
 cd $TOP/../
 
-cp -r ./vim_plugins/AfterColors ./
-cp -r ./vim_plugins/Align ./
-cp -r ./vim_plugins/calendar-vim ./
-cp -r ./vim_plugins/CountJump ./
-cp -r ./vim_plugins/cvim ./
-cp -r ./vim_plugins/fencview ./
-cp -r ./vim_plugins/FindMate ./
-cp -r ./vim_plugins/mark.vim ./
-cp -r ./vim_plugins/matchit ./
-# cp -r ./vim_plugins/omnicppcomplete ./
-cp -r ./vim_plugins/project ./
-cp -r ./vim_plugins/python_match ./
-cp -r ./vim_plugins/repeat ./
-cp -r ./vim_plugins/showmarks ./
-cp -r ./vim_plugins/surround ./
-cp -r ./vim_plugins/taglist ./
-cp -r ./vim_plugins/vim-autocomplpop ./
-cp -r ./vim_plugins/vim-fuzzyfinder ./
-cp -r ./vim_plugins/vim-l9 ./
-cp -r ./vim_plugins/ZoomWin ./
-
+if [ "$SYSTEM_TYPE" == "Linux" ]
+then
+	ln -s $TOP/vim_plugins/AfterColors ./
+	ln -s $TOP/vim_plugins/Align ./
+	ln -s $TOP/vim_plugins/calendar-vim ./
+	ln -s $TOP/vim_plugins/CountJump ./
+	ln -s $TOP/vim_plugins/cvim ./
+	ln -s $TOP/vim_plugins/fencview ./
+	ln -s $TOP/vim_plugins/FindMate ./
+	ln -s $TOP/vim_plugins/mark.vim ./
+	ln -s $TOP/vim_plugins/matchit ./
+	# ln -s $TOP/vim_plugins/omnicppcomplete ./
+	ln -s $TOP/vim_plugins/project ./
+	ln -s $TOP/vim_plugins/python_match ./
+	ln -s $TOP/vim_plugins/repeat ./
+	ln -s $TOP/vim_plugins/showmarks ./
+	ln -s $TOP/vim_plugins/surround ./
+	ln -s $TOP/vim_plugins/taglist ./
+	ln -s $TOP/vim_plugins/vim-autocomplpop ./
+	ln -s $TOP/vim_plugins/vim-fuzzyfinder ./
+	ln -s $TOP/vim_plugins/vim-l9 ./
+	ln -s $TOP/vim_plugins/ZoomWin ./
+else
+	cp -r $TOP/vim_plugins/AfterColors ./
+	cp -r $TOP/vim_plugins/Align ./
+	cp -r $TOP/vim_plugins/calendar-vim ./
+	cp -r $TOP/vim_plugins/CountJump ./
+	cp -r $TOP/vim_plugins/cvim ./
+	cp -r $TOP/vim_plugins/fencview ./
+	cp -r $TOP/vim_plugins/FindMate ./
+	cp -r $TOP/vim_plugins/mark.vim ./
+	cp -r $TOP/vim_plugins/matchit ./
+	# cp -r $TOP/vim_plugins/omnicppcomplete ./
+	cp -r $TOP/vim_plugins/project ./
+	cp -r $TOP/vim_plugins/python_match ./
+	cp -r $TOP/vim_plugins/repeat ./
+	cp -r $TOP/vim_plugins/showmarks ./
+	cp -r $TOP/vim_plugins/surround ./
+	cp -r $TOP/vim_plugins/taglist ./
+	cp -r $TOP/vim_plugins/vim-autocomplpop ./
+	cp -r $TOP/vim_plugins/vim-fuzzyfinder ./
+	cp -r $TOP/vim_plugins/vim-l9 ./
+	cp -r $TOP/vim_plugins/ZoomWin ./
+fi
 
 
--- uninstall.sh
+++ uninstall.sh
@@ -4,26 +4,26 @@ TOP=$(pwd)
 
 cd $TOP/../
 
-rm -rf ./AfterColors
-rm -rf ./Align
-rm -rf ./calendar-vim
-rm -rf ./CountJump
-rm -rf ./cvim
-rm -rf ./fencview
-rm -rf ./FindMate
-rm -rf ./mark.vim
-rm -rf ./matchit
-# rm -rf ./omnicppcomplete
-rm -rf ./project
-rm -rf ./python_match
-rm -rf ./repeat
-rm -rf ./showmarks
-rm -rf ./surround
-rm -rf ./taglist
-rm -rf ./vim-autocomplpop
-rm -rf ./vim-fuzzyfinder
-rm -rf ./vim-l9
-rm -rf ./ZoomWin
+rm -rf $TOP/AfterColors
+rm -rf $TOP/Align
+rm -rf $TOP/calendar-vim
+rm -rf $TOP/CountJump
+rm -rf $TOP/cvim
+rm -rf $TOP/fencview
+rm -rf $TOP/FindMate
+rm -rf $TOP/mark.vim
+rm -rf $TOP/matchit
+# rm -rf $TOP/omnicppcomplete
+rm -rf $TOP/project
+rm -rf $TOP/python_match
+rm -rf $TOP/repeat
+rm -rf $TOP/showmarks
+rm -rf $TOP/surround
+rm -rf $TOP/taglist
+rm -rf $TOP/vim-autocomplpop
+rm -rf $TOP/vim-fuzzyfinder
+rm -rf $TOP/vim-l9
+rm -rf $TOP/ZoomWin
 
 
 
